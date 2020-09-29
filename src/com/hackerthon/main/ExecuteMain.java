4b41e7e5763eeeb49ae4ce4f3141731bd0b36f82
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-09-29 23:27:15 +0530

    remove comments in main java file

--- src/com/hackerthon/main/ExecuteMain.java
+++ src/com/hackerthon/main/ExecuteMain.java
@@ -3,21 +3,9 @@ package com.hackerthon.main;
 import com.hackerthon.common.UtilTRANSFORM;
 import com.hackerthon.service.EmployeeServiceImpl;
 
-import java.util.logging.Logger;
-
 public class ExecuteMain {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-
-		/******************** Mm danna log line add karanne mehema ***********************/
-		Logger LOGGER = Logger.getLogger(ExecuteMain.class.getName());
-
-//		LOGGER.info("Sample info msg");
-//		LOGGER.warning("Sample warning msg");
-		/********************************************/
 
 		EmployeeServiceImpl employeeService = new EmployeeServiceImpl();
 		try {
@@ -25,10 +13,8 @@ public class ExecuteMain {
 			employeeService.employeesFromXML();
 			employeeService.createEmployeesTable();
 			employeeService.addEmployee();
-//			employeeService.displayEmployee();
+			employeeService.displayEmployee();
 		} catch (Exception e) {
 		}
-
 	}
-
 }
