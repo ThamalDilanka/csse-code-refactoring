2dcc2d7aa3de36c67f32cbbe08c4cd1ca4f1ded5
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-10-02 12:31:47 +0530

    refined

--- src/com/hackerthon/main/ExecuteMain.java
+++ src/com/hackerthon/main/ExecuteMain.java
@@ -6,10 +6,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 
+/**
+ * The class contains main method
+ * 
+ * @author Thamal Dilanka
+ * @version 1.1
+ */
+
 public class ExecuteMain {
 
 	public static void main(String[] args) {
-
+/**
+ * entry point
+ * with instance to EmployeeServiceImpl
+ */
 		final Logger log = Logger.getLogger(TransformUtil.class.getName());
 		
 		EmployeeService employeeService = new EmployeeService();
